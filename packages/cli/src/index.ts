a7aabeb6ff7416ed66fe5d6ef06e6955c2f45ad2
Harsh <hvs.231918@gmail.com>
2022-07-08 13:00:29 +0530

    Initial commit

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,10 @@
+#!usr/bin/env node
+// const serve = require("local-api");
+// serve();
+
+import {program} from 'commander';
+import {serveCommand} from './commands/serve';
+
+program.addCommand(serveCommand);
+
+program.parse(process.argv);
